437afd337659f97248d788ac558bde2661ed3f98
Jimenez Juan <jimenez.f.juan.m@gmail.com>
2024-12-14 22:44:54 -0400

    refactor(tickets): migrate ticket retrieval to Prisma and remove initialTickets
    - Deleted the initialTickets data file.
    - Updated getTicket and getTickets functions to fetch data from the Prisma database instead of using hardcoded initial data.
    - Introduced prisma.ts for Prisma client initialization.

--- src/app/tickets/queries/get-ticket.ts
+++ src/app/tickets/queries/get-ticket.ts
@@ -1,12 +1,10 @@
-import { initialTickets } from "@/data";
 import { Ticket } from "@/features/ticket/types";
+import { prisma } from "@/lib/prisma";
 
 export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  const maybeTicket = initialTickets.find((ticket) => ticket.id === ticketId);
-
-  return new Promise((resolve) => {
-    resolve(maybeTicket || null);
+  return prisma.ticket.findUnique({
+    where: {
+      id: ticketId,
+    },
   });
 };
--- src/app/tickets/queries/get-tickets.ts
+++ src/app/tickets/queries/get-tickets.ts
@@ -1,10 +1,6 @@
-import { initialTickets } from "@/data";
 import { Ticket } from "@/features/ticket/types";
+import { prisma } from "@/lib/prisma";
 
 export const getTickets = async (): Promise<Ticket[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  return new Promise((resolve) => {
-    resolve(initialTickets);
-  });
+  return prisma.ticket.findMany();
 };
