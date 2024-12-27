63207a62580c1d3151a596d08c4d71114f94cd90
Jimenez Juan <jimenez.f.juan.m@gmail.com>
2024-12-27 10:58:55 -0400

    feat: add `RedirectToast` component to display toast messages on redirect
    - Introduced a new `RedirectToast` component to handle toast messages based on cookies.
    - Integrated `RedirectToast` into tickets listing and detail pages.
    - Enhanced user feedback by showing success toasts after specific actions.

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -20,7 +20,7 @@ const upsertTicketSchema = z.object({
 export const upsertTicket = async (
   id: string | undefined,
   _actionState: ActionState,
-  formData: FormData,
+  formData: FormData
 ) => {
   try {
     const data = upsertTicketSchema.parse({
