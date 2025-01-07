3f9021a644bb7f191f3e7af5c811d5f57ea3d165
Jimenez Juan <jimenez.f.juan.m@gmail.com>
2025-01-06 22:41:55 -0400

    feat(utils): add getBaseUrl function- Create getBaseUrl function in src/utils/url.ts - Return appropriate base URL based on environment (development or production)- Use Vercel URL for production environment

---
+++ src/utils/url.ts
@@ -0,0 +1,7 @@
+export const getBaseUrl = () => {
+  const environment = process.env.NODE_ENV;
+
+  return environment === "development"
+    ? "http://localhost:3000"
+    : `${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+};
