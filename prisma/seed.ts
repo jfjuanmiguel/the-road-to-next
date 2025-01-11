65cb6545497bfd77b9ee0dbbd43d357aa90f1390
Jimenez Juan <jimenez.f.juan.m@gmail.com>
2025-01-11 13:53:07 -0400

    feat(prisma): add user-tickets relationship
    - Update schema.prisma to include tickets array in User model and userId field in Ticket model with relation
    - Modify seed.ts to create users and associate them with tickets
    - Import hash function for password hashing in seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,18 @@
+import { hash } from "@node-rs/argon2";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+const users = [
+  {
+    username: "admin",
+    email: "admin@admin.com",
+  },
+  {
+    username: "user",
+    email: "juan@jmez.dev",
+  },
+];
 
 const tickets = [
   {
@@ -31,9 +43,22 @@ const seed = async () => {
   console.log("DB Seed: Started ...");
 
   await prisma.ticket.deleteMany({});
+  await prisma.user.deleteMany({});
+
+  const passwordHash = await hash("Pass1234");
+
+  const dbUsers = await prisma.user.createManyAndReturn({
+    data: users.map((user) => ({
+      ...user,
+      passwordHash,
+    })),
+  });
 
   await prisma.ticket.createMany({
-    data: tickets,
+    data: tickets.map((ticket) => ({
+      ...ticket,
+      userId: dbUsers[0].id,
+    })),
   });
 
   const t1 = performance.now();
